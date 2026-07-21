579d71bb6aaa07171aef16378297e0bb5e799068
Sam <samu3lf3lixx@gmail.com>
2026-07-21 20:15:02 -0300

    pagina dinamica

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,8 @@ import { Suspense } from "react";
 export default async function HomePage() {
   return (
     <>
-      <Suspense fallback={<SpinLoader></SpinLoader>}>
+      <Suspense fallback={<SpinLoader className="min-h-1 mb-10"></SpinLoader>}>
         <PostFeatured></PostFeatured>
-      </Suspense>
-
-      <Suspense fallback={<SpinLoader></SpinLoader>}>
         <PostList></PostList>
       </Suspense>
     </>
--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,17 +1,32 @@
+import { SinglePost } from "@/src/Components/SinglePost";
+import { SpinLoader } from "@/src/Components/SpinLoader";
 import { findPostBySlugCached } from "@/src/lib/post/queries";
+import { Metadata } from "next";
+import { Suspense } from "react";
 
 type PostSlugPageProps = {
   params: Promise<{ slug: string }>;
 };
 
-export default async function PostSlugPage({ params }: PostSlugPageProps) {
+export async function generateMetadata({
+  params,
+}: PostSlugPageProps): Promise<Metadata> {
   const { slug } = await params;
 
   const post = await findPostBySlugCached(slug);
 
+  return {
+    title: post.title,
+    description: post.excerpt,
+  };
+}
+
+export default async function PostSlugPage({ params }: PostSlugPageProps) {
+  const { slug } = await params;
+
   return (
-    <div>
-      <p>{post.content}</p>
-    </div>
+    <Suspense fallback={<SpinLoader></SpinLoader>}>
+      <SinglePost slug={slug}></SinglePost>
+    </Suspense>
   );
 }
