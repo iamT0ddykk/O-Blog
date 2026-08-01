04df53ae92f7557005371a81fc4d776054f03a02
Sam <samu3lf3lixx@gmail.com>
2026-07-31 21:14:39 -0300

    adm panel comecado

---
+++ src/app/admin/login/page.tsx
@@ -0,0 +1,9 @@
+export const dynamic = "force-dynamic";
+
+export default async function AdminLoginPage() {
+  return (
+    <>
+      <div className="text-4xl py-16">oi</div>
+    </>
+  );
+}
---
+++ src/app/admin/post/page.tsx
@@ -0,0 +1,9 @@
+export const dynamic = "force-dynamic";
+
+export default async function AdminPostPage() {
+  return (
+    <>
+      <div className="text-4xl py-16">postpage</div>
+    </>
+  );
+}
