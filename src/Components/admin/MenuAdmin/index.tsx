636c85f82d848fe05426b390e133aa16d79049d9
Sam <samu3lf3lixx@gmail.com>
2026-08-14 23:27:32 -0300

    adm nav style

--- src/Components/admin/MenuAdmin/index.tsx
+++ src/Components/admin/MenuAdmin/index.tsx
@@ -1,14 +1,22 @@
+import clsx from "clsx";
 import Link from "next/link";
 
 export function MenuAdmin() {
+  const navClasses = clsx(
+    "flex gap-5 text-3xl bg-slate-900 text-slate-100 rounded-lg",
+  );
+  const linkClasses = clsx("");
+
   return (
     <>
-      <nav className="flex gap-5 text-3xl">
+      <nav className={navClasses}>
         <a href="/" target="_blank">
           Home
         </a>
 
-        <Link href={"/admin/post"}>Posts</Link>
+        <Link className={linkClasses} href={"/admin/post"}>
+          Posts
+        </Link>
       </nav>
     </>
   );
