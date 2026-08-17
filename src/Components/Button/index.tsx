13c3583140e7e76266e44609ff639da5454956c4
Sam <samu3lf3lixx@gmail.com>
2026-08-17 18:56:56 -0300

    variantes add

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import clsx from "clsx";
+
+type ButtonVariants = "default" | "ghost" | "danger";
+type ButtonSizes = "sm" | "md" | "lg";
 
 type ButtonProps = {
-  children: React.ReactNode;
+  variant?: ButtonVariants;
+  size?: ButtonSizes;
 } & React.ComponentProps<"button">;
 
-export function Button({ children }: ButtonProps) {
-  return <></>;
+export function Button({
+  variant = "default",
+  size = "md",
+  ...props
+}: ButtonProps) {
+  const buttonVariants: Record<ButtonVariants, string> = {
+    default: clsx("bg-blue-600 text-blue-100"),
+    ghost: clsx("bg-slate-200 text-slate-900"),
+    danger: clsx("bg-red-600 text-red-100"),
+  };
+
+  const buttonSizes: Record<ButtonSizes, string> = {
+    sm: clsx(""),
+    md: clsx(""),
+    lg: clsx(""),
+  };
+
+  const buttonClasses = clsx(buttonVariants[variant], buttonSizes[size]);
+
+  return <button className={buttonClasses} {...props} />;
 }
--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,9 +1,15 @@
+import { Button } from "@/src/Components/Button";
+
 export const dynamic = "force-dynamic";
 
 export default async function AdminPostNewPage() {
   return (
     <>
-      <div className="text-4xl py-16">adm new</div>
+      <div className="text-4xl flex gap-3 py-16">
+        <Button variant="default">texto</Button>
+        <Button variant="ghost">texto</Button>
+        <Button variant="danger">texto</Button>
+      </div>
     </>
   );
 }
