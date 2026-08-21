bee8d170fdeeafcfe09f118713feea17af86cfc7
Sam <samu3lf3lixx@gmail.com>
2026-08-21 16:22:21 -0300

    markdownconfig

--- src/Components/admin/ManagePostForm/index.tsx
+++ src/Components/admin/ManagePostForm/index.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import { useState } from "react";
 import { Button } from "../../Button";
 import { InputCheckbox } from "../../InputCheckbox";
 import { InputText } from "../../InputText";
+import { MarkdownEditor } from "../../MarkdownEditor";
 
 export function ManagePostForm() {
+  const [value, setValue] = useState("a");
+
   return (
     <form action="" className="mb-16">
       <div className="flex flex-col gap-6">
@@ -39,6 +43,14 @@ export function ManagePostForm() {
           defaultValue="Olá mundo"
           readOnly
         />
+
+        <MarkdownEditor
+          labelText="counteud"
+          disabled={false}
+          textAreaName="content"
+          value={value}
+          setValue={setValue}
+        ></MarkdownEditor>
 
         <div className="mt-4">
           <Button type="submit">Enviar</Button>
