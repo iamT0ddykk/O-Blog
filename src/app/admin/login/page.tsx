1e10ad168fec5284257b84d78439b288097da581
Sam <samu3lf3lixx@gmail.com>
2026-09-17 17:26:45 -0300

    login page

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,9 +1,42 @@
+import { Button } from "@/src/Components/Button";
+import { InputText } from "@/src/Components/InputText";
+import clsx from "clsx";
+import { LogInIcon } from "lucide-react";
+
 export const dynamic = "force-dynamic";
 
 export default async function AdminLoginPage() {
   return (
     <>
-      <div className="text-4xl py-16">oi</div>
+      <div
+        className={clsx(
+          "flex items-center justify-center",
+          "text-center max-w-sm mt-16 mb-32 mx-auto",
+        )}
+      >
+        <form className="flex-1 flex flex-col gap-6">
+          <InputText
+            type="text"
+            name="username"
+            labelText="Usuário"
+            placeholder="Seu usuário"
+
+          />
+
+          <InputText
+            type="password"
+            name="password"
+            labelText="Senha"
+            placeholder="Sua senha"
+          />
+
+          <Button type="submit" className="mt-4">
+            <LogInIcon />
+            Entrar
+          </Button>
+
+        </form>
+      </div>
     </>
   );
 }
