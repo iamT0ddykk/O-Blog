839dac9f0791a2af92c94374f424194e2198bced
Sam <samu3lf3lixx@gmail.com>
2026-09-22 18:10:21 -0300

    hashgenerator

--- src/lib/login/manage-login.ts
+++ src/lib/login/manage-login.ts
@@ -2,16 +2,17 @@ import bcrypt from "bcryptjs";
 
 export async function hashPassword(password: string) {
   const hash = await bcrypt.hash(password, 10);
-
-  return hash;
+  const b64 = await Buffer.from(hash).toString("base64");
+  return b64;
 }
 
-export async function verifyHash(password: string, hash: string) {
+export async function verifyPassword(password: string, base64hash: string) {
+  const hash = await Buffer.from(base64hash, "base64").toString("utf-8");
   const isValid = await bcrypt.compare(password, hash);
   return isValid;
 }
 
 (async () => {
-  const hashedPass = await hashPassword("1234");
+  const hashedPass = await hashPassword("123");
   console.log({ hashedPass });
 })();
